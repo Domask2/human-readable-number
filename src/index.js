c3f5d904fa722fe795e5a88816865680a482c230
Domask <domask2@mail.ru>
2020-09-29 16:32:26 +0400

    feat:add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,33 @@
 module.exports = function toReadable (number) {
+    let result = '';
+    const teenth = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 
+                    'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 
+                    'eighteen', 'nineteen'];
+    const tens = ['twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+    
+  if(number>=0 && number <20) {
+        result += `${teenth[number]}`
+    } else if (number>=20 && number <100) {
+        result += `${tens[Math.floor(number%100/10)-2]} ${number%10!=0 ? teenth[Math.floor(number%10)]:''}`
+    } else if(number>=100 && number <1000) {
+      
+         result += `${teenth[Math.floor(number/100)]} hundred ` 
+        
+         if(number%100 < 20) {
+           result += `${teenth[Math.floor(number%100)]}`
+        } else  {
+           result += `${tens[Math.floor(number%100/10)-2]} ${number%10!=0? teenth[number%10]: ''}`
+        } 
+      
+      if(number%100 === 0) {
+        return `${teenth[Math.floor(number/100)]} hundred`
+      }
+      
   
+    } 
+  
+    
+    
+    return result.trim('');
+    
 }
